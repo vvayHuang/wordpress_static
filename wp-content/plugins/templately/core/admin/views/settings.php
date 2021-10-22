90e1d4dbb59639f6b5721d1a31160b9827cbc71d
Clancies <md46343113@msn.com>
2021-10-22 22:18:12 +0800

    relative

--- wp-content/plugins/templately/core/admin/views/settings.php
+++ wp-content/plugins/templately/core/admin/views/settings.php
@@ -1,7 +1,7 @@
 <div class="templately-admin-body">
     <div id="templatelyAdmin">
         <div class="templately-admin-preloader" style="padding: 100px;min-height: 100%;box-sizing: border-box;text-align: center;">
-            <img width="130px" src="http://clancies.github.io/gbzzz/wp-content/plugins/templately/core/admin/views/TEMPLATELY_ASSETSimages/loading-logo.gif" alt="">
+            <img width="130px" src="/path/wp-content/plugins/templately/core/admin/views/TEMPLATELY_ASSETSimages/loading-logo.gif" alt="">
         </div>
     </div>
 </div>
